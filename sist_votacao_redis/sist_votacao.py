0f817d149b69eb2310d7093f98071822b6ec29a0
lvdamaceno <lvdamaceno@gmail.com>
2018-06-03 16:09:15 -0300

    docstrings

--- sist_votacao_redis/sist_votacao.py
+++ sist_votacao_redis/sist_votacao.py
@@ -3,15 +3,30 @@ import random
 
 r = redis.StrictRedis(host='localhost', port=6379,
                       charset="utf-8", decode_responses=True, db=0)
+# limpa o banco
 r.flushall()
 
 
 def qtd_votos(limite):
+    """
+    Cria o range de votações que serão executadas,
+    de 1 até o limite informado.
+    :param limite: limite de votos que podem ser calculados
+    :return: um int da quantidade calculada
+    """
     qtd = int(random.randint(1, limite))
     return qtd
 
 
 def votacao(qtd, id_inicio, id_final):
+    """
+    Povoa o banco com valores de votação randomicos, cria os valores
+    id de usuarios ficticios, visto um range passado e seta no banco ao final
+    :param qtd: um int com a quantidade de votos à serem gerados
+    :param id_inicio: int com um valor inicial qualquer
+    :param id_final:  int com um valor final qualquer
+    :return: none
+    """
     for i in range(qtd):
         user = str(random.randint(id_inicio, id_final))
         voto = int(int(random.randint(1, 2)))
@@ -20,6 +35,14 @@ def votacao(qtd, id_inicio, id_final):
 
 
 def apuracao():
+    """
+    inicializa duas variaveis para receber a contagem de votos para
+    c1 e c2, count recebe o tamanho do banco e allkeys os valores das
+    chaves em um lista. o laço for percorre allkeys e guarda o valor na
+    variavel voto e entra no laço if, se voto for um itera em c1,
+    do contrario itera em c2.
+    :return: um list contendo a qtd de votos para c1 e c2
+    """
     c1 = 0
     c2 = 0
     count = int(r.dbsize())
@@ -36,7 +59,12 @@ def apuracao():
 
 
 def resultado(c1, c2):
-    # faz a comparação entre os votos e diz quem foi o vencedor.
+    """
+    Faz a comparação entre os votos e diz quem foi o vencedor.
+    :param c1: int, quantidade de votos computados para c1
+    :param c2: int, quantidade de votos computados para c2
+    :return: none
+    """
     if c1 == c2:
         print('Empatou, ' + str(c1) + ' = ' + str(c2))
     elif c1 > c2:
@@ -45,10 +73,21 @@ def resultado(c1, c2):
         print('Candidato 2 foi campeão com ' + str(c2) + ' votos contra ' + str(c1))
 
 
-qtd_votos = qtd_votos(100)
+"""
+Executa a votação, informando no primeiro parametro a função qtd_votos
+com valor 1000 e os delimitadores dos ids fakes, ao final guarda tudo 
+banco.
+"""
+votacao(qtd_votos(1000), 100000, 999999)
 
-votacao(qtd_votos, 100000, 999999)
 
+"""
+Efetua a apuração dos votos
+"""
 t = apuracao()
+print(t[0])
 
+"""
+Retorna o vencedor
+"""
 resultado(t[0], t[1])
