2632386a0eaf278c5480d42dd408bdfe6f80fbad
lvdamaceno <lvdamaceno@gmail.com>
2018-06-16 22:39:04 -0300

    refatoracao das funcoes, ciracao das funcoes de edicao dos valores das colunas na tabela

--- playlist_casandra/Users/main.py
+++
@@ -1,4 +0,0 @@
-from Users.random_users import generate, select_all, edit
-
-generate(2)
-select_all()
--- playlist_casandra/Users/random_users.py
+++ playlist_casandra/Users/random_users.py
@@ -7,11 +7,11 @@ import random
 from cassandra.cluster import Cluster
 
 cluster = Cluster()
+# criar o banco antes
 session = cluster.connect('aula')
 
 
-# utilities
-
+# funções de criação
 
 def create_table():
     # dropa a tabela
@@ -46,6 +46,8 @@ def generate(qtd):
         """.format(names.get_last_name(), idade, cidade, names.get_first_name()))
 
 
+# Funções de seleção
+
 # seleciona as linhas do banco
 def select_all():
     # executa os selects de teste
@@ -61,9 +63,68 @@ def select_all():
               user.id)
 
 
-# edita uma linha filtrando pelo ultimo nome, informando a coluna que será modificada e o novo valor
+# Funções de edição
+
+# edita uma linha inteira filtrando pelo ultimo nome, informando a coluna que será modificada e o novo valor
 def edit(last_name, coluna, valor):
     pessoa = session.execute("select * from users where lastname='{0}' allow filtering".format(last_name))
     for dados in pessoa:
         id = dados.id
         session.execute("update users set {1} = '{2}' where id = {0}".format(id, coluna, valor))
+    print("")
+
+
+# edita o valor da idade informado o id da pessoa e o novo valor
+def edit_age(id, valor):
+    pessoa = session.execute("select * from users where id={0} allow filtering".format(id))
+    for dados in pessoa:
+        id = dados.id
+        session.execute("update users set age = '{1}' where id = {0}".format(id, valor))
+    select_all()
+    print("")
+
+
+# edita o ultimo nome informado o id da pessoa e a correção
+def edit_first_name(id, firstname):
+    pessoa = session.execute("select * from users where id={0} allow filtering".format(id))
+    for dados in pessoa:
+        id = dados.id
+        session.execute("update users set firstname = '{1}' where id = {0}".format(id, firstname))
+    select_all()
+    print("")
+
+
+# edita o ultimo nome informado o id da pessoa e a correção
+def edit_last_name(id, lastname):
+    pessoa = session.execute("select * from users where id={0} allow filtering".format(id))
+    for dados in pessoa:
+        id = dados.id
+        session.execute("update users set lastname = '{1}' where id = {0}".format(id, lastname))
+        session.execute("update users set email = '{1}@gmail.com' where id = {0}".format(id, lastname))
+    select_all()
+    print("")
+
+
+# edita o nome inteiro informado o id da pessoa e a correção
+def edit_all_name(id, nome):
+    nome = nome.partition(" ")
+    firstname = nome[0]
+    lastname = nome[2]
+    pessoa = session.execute("select * from users where id={0} allow filtering".format(id))
+    for dados in pessoa:
+        id = dados.id
+        session.execute("update users set firstname = '{1}' where id = {0}".format(id, firstname))
+        session.execute("update users set lastname = '{1}' where id = {0}".format(id, lastname))
+        session.execute("update users set email = '{1}@gmail.com' where id = {0}".format(id, lastname))
+    select_all()
+    print("")
+
+
+# edita o email informado o id da pessoa e a correção
+def edit_email(id, mail):
+    pessoa = session.execute("select * from users where id={0} allow filtering".format(id))
+    for dados in pessoa:
+        id = dados.id
+        session.execute("update users set email = '{1}' where id = {0}".format(id, mail))
+    select_all()
+    print("")
